c8479884529d67893e27c960ef8fc9fe11ae32d3
suochao <suochao.chen@tuya.com>
2022-07-27 14:25:06 +0800

    恢复1.8编译

--- src/main/java/com/tencent/wxcloudrun/controller/ArticleController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ArticleController.java
@@ -2,12 +2,14 @@ package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.utils.HttpUtils;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author suochao
  * @date 2022/7/27 1:27 下午
  */
+@Controller
 public class ArticleController {
 
 
@@ -20,7 +22,7 @@ public class ArticleController {
      *
      * @return
      */
-    @PostMapping(value = "/article/list")
+    @GetMapping(value = "/article/list")
     public ApiResponse list() throws Exception {
 
         String s = HttpUtils.get(ARTICLE_LIST_URL);
